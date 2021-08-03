806c481e2156a64b02c6b69bf2496e306dc6780b
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-03 20:22:39 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -1,2 +1,9 @@
 # Faça um programa que pergunte quanto você ganha por hora e o numero de horas trabalhadas no mes
 # Calcule e mostre o total do seu salario.
+
+valor = input ( "Quanto voce ganha por hora? ")
+horas = input ( "Quantas horas voce trabalha no mes? ")
+
+calculo = int (valor) * int (horas)
+
+print (" Meu salario no mes e: " + str (calculo))
