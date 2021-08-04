6e93d5c230f97450d4083606ef2de7a63bab70da
Shirley Baltar <88407988+ShirleyBaltar@users.noreply.github.com>
2021-08-03 21:03:11 -0300

    Create aula12.py
    feito

---
+++ aula12.py
@@ -0,0 +1,7 @@
+# Faça um Programa que converta metros para centímetros.
+
+metros = input("digite quantos metros voce tem?")
+
+multiplicaçao = int(metros) * 100
+
+print("resultado é " + str(multiplicaçao) + " cm")
