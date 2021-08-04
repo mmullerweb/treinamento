0f69b65857363827b1b2a299e6b7a36964940d82
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-03 21:10:40 -0300

    feito
    feito

--- aula12.py
+++ aula12.py
@@ -1,1 +1,7 @@
 # Faça um Programa que converta metros para centímetros.
+
+metros = input(" Digite uma medida em metros: ")
+
+converter = int (metros) * 100
+
+print ("converter em centimetros: " + str (converter))
--- aula13.py
+++ aula13.py
@@ -1,1 +1,8 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = input ("Qual o valor do raio? ")
+
+circulo = int (raio) * 2 * 3
+
+print (" A area do circulo e: " + str (circulo))
+
