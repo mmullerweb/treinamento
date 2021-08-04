73b6461206417127b7f751a8109f6c158bb1153e
Shirley Baltar <88407988+ShirleyBaltar@users.noreply.github.com>
2021-08-03 21:35:30 -0300

    Create aula13.py
    feito

---
+++ aula13.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = input ("Qual o valor do raio? ")
+
+circulo = int (raio) * 2 * 3
+
+print (" A area do circulo e: " + str (circulo))
