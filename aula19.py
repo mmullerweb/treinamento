6af8d752009d39405389c7966a42ddad2222bee1
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-05 20:02:55 -0300

    feito

---
+++ aula19.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça a nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
+# e continue pedindo até que informe um valor inválido.
+
+nota = 0
+while (nota >= 0 and nota <= 10):
+    nota = int (input("Informe a nota... "))
+print ("Nota nao valida")
