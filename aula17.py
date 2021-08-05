68521824217b3e2d2f679c2675ef363e05d43acc
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-05 18:03:58 -0300

    Feito

--- aula17.py
+++ aula17.py
@@ -1,1 +1,12 @@
 # Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+sexo = input ("Digite sexo F ou M: ")
+
+if str (sexo) == ("F"): 
+    print ("Feminino") 
+
+elif str (sexo) == ("M"): 
+    print ("Masculino")
+
+else:
+    print ("Sexo invalido")
