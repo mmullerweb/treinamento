9164cb82d9e6fb6d991acfe89c25b3e4ab9647a2
Martin Müller <mmuller.web@gmail.com>
2021-08-05 20:01:00 -0300

    function
    def ou function

--- aula19.py
+++ aula19.py
@@ -1,4 +1,17 @@
 # Faça um programa que peça a nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
 # e continue pedindo até que informe um valor inválido.
+def teste():
+    nota = 0
+    while(nota >= 0 and nota <= 10):
+        nota = int(input("Informe a nota... "))
+    print("Nota foi inválida... ")
+#teste()
+#print(1)
 
+def soma(a,b):
+    return a + b
 
+def subtracao(a,b):
+    return a - b
+print(soma(1,5))
+print(subtracao(9,4))
