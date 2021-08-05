edfd3bc7cb357dbc62319ab9b3316b71f5da5f25
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-05 17:37:15 -0300

    feito

--- aula16.py
+++ aula16.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = input ("Digite um valor: ")
+
+if int (valor) >= 0 :
+    print ("Positivo")
+
+else:
+    print ("Negativo")
