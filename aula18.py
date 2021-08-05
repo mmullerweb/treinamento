d8a8a0eab2d62206bc5088dc94e1d1dade07479e
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-05 18:30:09 -0300

    feito

--- aula18.py
+++ aula18.py
@@ -1,1 +1,21 @@
 # Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+letra = input ("Digite uma letra: ")
+
+if letra == ("a"):
+    print ("vogal")
+    
+elif letra == ("e"):
+    print ("vogal")
+
+elif letra == ("i"):
+    print ("vogal")
+
+elif letra == ("o"):
+    print ("vogal")
+
+elif letra == ("u"):
+    print ("vogal")
+
+else:
+    print ("consoante")
