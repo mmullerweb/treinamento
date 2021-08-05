4c1a396970f7672febe0ceaa4b6869918ab694f6
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-05 17:23:11 -0300

    feito

--- aula15.py
+++ aula15.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça dois números e imprima o maior deles.
+num1 = input ("Digite um numero: ")
+num2 = input ("Digite outro numero: ")
+
+if int (num1) > int (num2): 
+    print (num1)
+
+else:
+    print (num2)
