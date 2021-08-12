03172385245a9da6907a9da4ffe309dea182db1d
VivianeRomi <88408298+VivianeRomi@users.noreply.github.com>
2021-08-12 19:44:03 -0300

    Update aula22.py
    Feito

--- aula22.py
+++ aula22.py
@@ -1,9 +1,19 @@
 # Instalar lib flask dentro do windows 
 ######
+import pymysql
 from flask import Flask, request
 
 app = Flask(__name__)
 
+conn = pymysql.connect(host="mysql.by4u.com.br",user='by4u02',password='Treina2021',database='by4u02',cursorclass=pymysql.cursors.DictCursor)
+cursor = conn.cursor()
+
+
 @app.route("/pessoa/criar")
 def criar():
-    return ""
+    nome = request.args.get("nome")
+    data_nascimento = request.args.get("data_nascimento")
+    sql = "INSERT INTO pessoa(nome, data_nascimento)VALUES(%s,%s)"
+    cursor.execute(sql,(nome,data_nascimento))
+    conn.commit()
+    return "OK"
