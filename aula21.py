941234798a72e6a43abbb42e595605bc9fe66509
Martin Müller <mmuller.web@gmail.com>
2021-08-12 19:13:54 -0300

    teste
    Enviar

--- aula21.py
+++ aula21.py
@@ -12,22 +12,22 @@ def soma():
     soma = int(a) + int(b)
     return "Soma é: " + str(soma)
 
-@app.route("/subtracao")
+@app.route("/subtracao", methods=["POST"])
 def subtracao():
-    a = request.args.get("a")
-    b = request.args.get("b")
+    a = request.form.get("a")
+    b = request.form.get("b")
     subtracao = int(a) - int(b)
     return "Subtracao é: " + str(subtracao)
 
 @app.route("/multiplicacao")
-def subtracao():
+def multiplicacao():
     a = request.args.get("a")
     b = request.args.get("b")
     multiplicacao = int(a) * int(b)
     return "multiplicacao é: " + str(multiplicacao)
 
 @app.route("/divisao")
-def subtracao():
+def divisao():
     a = request.args.get("a")
     b = request.args.get("b")
     divisao = int(a) * int(b)
